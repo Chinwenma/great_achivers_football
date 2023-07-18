a1fc5652402909c46cee8d03611c6e9a56aac91c
Chinwenma <okoriechinwe1327@gmail.com>
2023-07-18 06:33:30 -0800

    first

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js,php}"],
+  theme: {
+    extend: {
+      colors:{
+       lightColor:"hsl(225, 24%, 87%)",
+       semiDarkColor:"hsl()",
+       
+      }
+    },
+  },
+  plugins: [],
+}
+
