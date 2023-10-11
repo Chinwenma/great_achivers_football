d36b499b1e1c109d34e6cf7af04aafe32c427f10
Chinwenma <okoriechinwe1327@gmail.com>
2023-10-10 20:20:26 -0800

    re

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,20 +1,46 @@
-<header>
-    <div class="logo-container">
-      <img src="./assets/logo.png" alt="Logo">
-      <span class="logo-text">Great Achivers</span>
-    </div>
-    <nav>
-      <ul class=" nav-links">
-        <li class="nav-item"><a href="index.html" class="nav-link">Home</a></li>
-        <li><a href="./pages/about.html">About</a></li>
-        <li><a href="./pages/gallery.html">Gallery</a></li>
-        <li><a href="./pages/gallery.html">Contact</a></li>
+<!-- <nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="./assets/img/logo.png" alt="Logo" width="40" height="34" class="d-inline-block align-text-top">
+      Great Achivers
+    </a>
+  </div>
+</nav> -->
+
+<nav class="navbar navbar-expand-lg header">
+  <div class="container">
+    <!-- Logo and Brand -->
+    <a class="navbar-brand" href="#">
+      <img src="./assets/img/logo.png" alt="Logo" width="40" height="34" class="d-inline-block align-text-center "> <span class="logo_text ">Great Achivers</span> 
+    </a>
+
+    <!-- Toggle Button (for small screens) -->
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- Navigation Links -->
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav mx-auto">
+        <li class="nav-item">
+          <a class="nav-link logo_text" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link logo_text" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link logo_text" href="#">Gallery</a>
+        </li>
+       
+        <li class="nav-item">
+          <a class="nav-link logo_text" href="#">Contact</a>
+        </li>
       </ul>
-    </nav>
-    <button class="login-btn">Partner with us</button>
-    <div class="hamburger">
-      <span class="hamburger-line"></span>
-      <span class="hamburger-line"></span>
-      <span class="hamburger-line"></span>
     </div>
-  </header>
+
+    <!-- Login Button -->
+    <div class="ml-auto">
+      <button class="btn CTA">Partner with us</button>
+    </div>
+  </div>
+</nav>
